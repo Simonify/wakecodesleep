d2d8801892c4c739398f6cf2e8ef34999bc1fa0d
Simon Fletcher <simon@wakecodesleep.com>
2016-05-02 03:24:16 -0700

    README.

---
+++ README.md
@@ -0,0 +1,4 @@
+wakecodesleep
+=============
+
+A blogging application used to power my personal website: http://wakecodesleep.com.
--- posts/2016-04-01-sup.js
+++
@@ -1,6 +0,0 @@
-export default {
-  title: "sup",
-  contents: "",
-  slug: "2016-04-01-sup",
-  created_at: new Date("2016-05-02T10:12:45.093Z")
-}  
